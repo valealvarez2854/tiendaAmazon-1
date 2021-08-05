ec52e09f1ce328c5be682bdee4ccbc7f23049eb1
Juan <juan.gallegomesa@gmail.com>
2021-08-05 16:56:27 -0500

    se crea modal

---
+++ controlador.js
@@ -0,0 +1,84 @@
+//CONTROLAR LA ACTIVIDAD DEL FORMULARIO
+let nombreConsola;
+let precioConsola;
+let pesoConsola;
+let fotoConsola;
+
+//1. Controlar con JS el boton del formulario
+let boton=document.getElementById("boton");
+
+//2. Detectar el clic en el boton
+boton.addEventListener("click",capturarDatos); 
+
+//3. Crear funcion PROPIA que se llamo en el punto 2
+function capturarDatos(){
+
+    //4. Capturar los valores del formulario
+    // creo tantas variables como valores del formulario
+
+    let consolaElegida=document.getElementById("productos").value;
+    seleccionarConsola(consolaElegida);
+
+    let cantidad=document.getElementById("cantidad").value;
+    
+    let pildora=document.getElementById("pildora");
+    pildora.textContent=cantidad;
+    pildora.classList.remove("invisible");
+    pildora.classList.add("visible");
+
+    
+    let direccion=document.getElementById("comentarios").value;
+    
+    let fotoCarrito=document.getElementById("fotoCarrito");
+    fotoCarrito.src=fotoConsola;
+
+    let tituloCarrito=document.getElementById("tituloCarrito");
+    tituloCarrito.textContent=nombreConsola;
+   
+    
+
+
+}
+
+//5. Seleccionar la información de la consola segun la selección del usuario
+function seleccionarConsola(opcion){
+
+    let consolas={
+        nombres:Array("PS5","XBOX Serie X", "Nintendo Switch"),
+        precios:Array(750,700,409),
+        pesos:Array(14.2,13.1,4.75),
+        fotos:Array("https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/ps5.jpg?raw=true","https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/xbox.jpg?raw=true","https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/nintendo.jpg?raw=true"),
+        
+    }
+
+    if(opcion==1){
+
+        nombreConsola=consolas.nombres[0];
+        precioConsola=consolas.precios[0];
+        pesoConsola=consolas.pesos[0];
+        fotoConsola=consolas.fotos[0];
+
+    }else if(opcion==2){
+
+        nombreConsola=consolas.nombres[1];
+        precioConsola=consolas.precios[1];
+        pesoConsola=consolas.pesos[1];
+        fotoConsola=consolas.fotos[1];
+
+    }else if(opcion==3){
+
+        nombreConsola=consolas.nombres[2];
+        precioConsola=consolas.precios[2];
+        pesoConsola=consolas.pesos[2];
+        fotoConsola=consolas.fotos[2];
+
+    }else{
+        nombreConsola=null;
+        precioConsola=null;
+        pesoConsola=null;
+        fotoConsola=null;
+
+    }
+
+
+}
